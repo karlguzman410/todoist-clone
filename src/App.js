70a46ef67c7747755a944516a304eeaee91e919f
Karl Guzman <guzmankarl0410@gmail.com>
2021-02-16 13:02:03 -0800

    initial sidebar setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Content } from './components/layout/Content'
 import { Header } from './components/layout/Header'
 
 
 export const App = () => (
   <div >
     <Header />
+    <Content />
   </div>
 )
 
